4d7beb035268fa74e78b60463b0852a1d9c87649
SoraTenshi <dream@neoncity.dev>
2022-12-01 15:29:34 +0100

    Add part 2
    Sort imports
    
    Removed unused imports

--- day-1/main.hs
+++ day-1/main.hs
@@ -1,20 +1,24 @@
 module Main where
 
-import System.FilePath
-import Data.Text.Lazy (splitOn)
+import Data.List (sortBy)
 
 import qualified Data.Text as T
 
 readInputFile :: String -> [String]
 readInputFile cs = map T.unpack (T.splitOn (T.pack "\n\n") (T.pack cs))
 
-toCalories :: [String] -> [Int]
-toCalories = map ((sum . map read) . lines)
+toCalories :: [String] -> [[Int]]
+toCalories = map (map read . lines)
 
-solve :: [Int] -> Int
-solve = maximum 
+solve :: [[Int]] -> Int
+solve = maximum . map sum
+
+solve' :: [[Int]] -> String
+solve' = show . take 3 . sortBy (flip compare) . map sum
 
 main :: IO ()
 main = do
   contents <- readFile "./input.txt"
   print $ solve $ toCalories $ readInputFile contents
+  print $ solve' $ toCalories $ readInputFile contents
+
