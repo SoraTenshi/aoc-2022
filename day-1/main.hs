90cac714fdd83a58462eb65bbbc39f5ecfc733dd
SoraTenshi <dream@neoncity.dev>
2022-12-01 15:14:03 +0100

    Add day 1 part 1

--- day-1/main.hs
+++ day-1/main.hs
@@ -1,10 +1,20 @@
 module Main where
 
-import System.IO
 import System.FilePath
+import Data.Text.Lazy (splitOn)
 
-readInputFile :: FilePath -> IO [String]
-readInputFile = fmap lines . readFile
+import qualified Data.Text as T
+
+readInputFile :: String -> [String]
+readInputFile cs = map T.unpack (T.splitOn (T.pack "\n\n") (T.pack cs))
+
+toCalories :: [String] -> [Int]
+toCalories = map ((sum . map read) . lines)
+
+solve :: [Int] -> Int
+solve = maximum 
 
 main :: IO ()
-main = mapM_ putStrLn =<< readInputFile "./input.txt"
+main = do
+  contents <- readFile "./input.txt"
+  print $ solve $ toCalories $ readInputFile contents
