1b2c601efa69a0333b87a7ebd7f04178cbe9286f
SoraTenshi <dream@neoncity.dev>
2022-12-02 18:15:19 +0100

    Simplify day 3

--- day-3/main.kt
+++ day-3/main.kt
@@ -1,17 +1,13 @@
 import java.io.File
 import java.io.InputStream
 
-fun read_lines_from_file(path: String): Array<String> {
-    val inputStr = File(path).inputStream();
-    val lineList = mutableListOf<String>();
-    
-    inputStr.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it) } }
-    return lineList.toTypedArray();
+fun readLinesFromFile(path: String): List<String> {
+    return File(path).inputStream().bufferedReader().readLines()
 }
 
 fun main() {
-    val lines = read_lines_from_file("input.txt");
-    for(line in lines) {
-        println("${line}");
+    val lines = readLinesFromFile("input.txt")
+    for (line in lines) {
+        println(line)
     }
 }
