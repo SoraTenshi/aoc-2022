2194b27bf04f7b20a628f51e110df3c71d3c2ff9
SoraTenshi <dream@neoncity.dev>
2022-12-03 16:24:25 +0100

    Add Day 3 part 1
    Update also compile_run
    
    use val

--- day-3/compile_run.sh
+++ day-3/compile_run.sh
@@ -4,5 +4,4 @@ kotlinc main.kt -include-runtime -d main.jar
 java -jar main.jar
 
 rm ./main.jar
-rm ./MainKt.class
 rm -rf ./META-INF
--- day-3/main.kt
+++ day-3/main.kt
@@ -1,13 +1,42 @@
 import java.io.File
 import java.io.InputStream
 
+fun String.splitAtIndex(i: Int) = take(i) to substring(i)
+
+fun toPrio(c: Char): Int {
+    val addition = if (c.isUpperCase()) 27 else 1;
+    return c.lowercaseChar().code - 'a'.code + addition;
+}
+
+fun splitInMiddle(s: String): Pair<String, String> {
+    return s.splitAtIndex(s.length / 2);
+}
+
 fun readLinesFromFile(path: String): List<String> {
     return File(path).inputStream().bufferedReader().readLines()
+} 
+
+fun solve(strings: List<String>): Int {
+    val priorities: MutableList<Int> = mutableListOf();
+    val charSet: MutableSet<Char> = mutableSetOf();
+    for(s in strings) {
+        val (l, r) = splitInMiddle(s);
+        for(c in l) {
+            charSet.add(c);
+        }
+        for(c in r) {
+            if(charSet.contains(c)) {
+                priorities.add(toPrio(c));
+                break;
+            }
+        }
+        charSet.clear();
+    }
+    
+    return priorities.sum();
 }
 
 fun main() {
     val lines = readLinesFromFile("input.txt")
-    for (line in lines) {
-        println(line)
-    }
+    println("Part 1: ${solve(lines)}");
 }
