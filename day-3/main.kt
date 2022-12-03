810835504ce87ad9b0bf6abeef4e0f8c20526c86
SoraTenshi <dream@neoncity.dev>
2022-12-03 17:32:01 +0100

    Add Day 3 part 2
    - Sidenote - i am not happy with my solution for part 2

--- day-3/main.kt
+++ day-3/main.kt
@@ -1,42 +1,74 @@
 import java.io.File
-import java.io.InputStream
 
 fun String.splitAtIndex(i: Int) = take(i) to substring(i)
 
 fun toPrio(c: Char): Int {
-    val addition = if (c.isUpperCase()) 27 else 1;
-    return c.lowercaseChar().code - 'a'.code + addition;
+    val addition = if (c.isUpperCase()) 27 else 1
+    return c.lowercaseChar().code - 'a'.code + addition
 }
 
 fun splitInMiddle(s: String): Pair<String, String> {
-    return s.splitAtIndex(s.length / 2);
+    return s.splitAtIndex(s.length / 2)
 }
 
 fun readLinesFromFile(path: String): List<String> {
     return File(path).inputStream().bufferedReader().readLines()
-} 
+}
 
 fun solve(strings: List<String>): Int {
-    val priorities: MutableList<Int> = mutableListOf();
-    val charSet: MutableSet<Char> = mutableSetOf();
-    for(s in strings) {
-        val (l, r) = splitInMiddle(s);
-        for(c in l) {
-            charSet.add(c);
+    var prio = 0
+    val charSet: MutableSet<Char> = mutableSetOf()
+    for (s in strings) {
+        val (l, r) = splitInMiddle(s)
+        for (c in l) {
+            charSet.add(c)
         }
-        for(c in r) {
-            if(charSet.contains(c)) {
-                priorities.add(toPrio(c));
-                break;
+        for (c in r) {
+            if (charSet.contains(c)) {
+                prio += toPrio(c)
+                break
             }
         }
-        charSet.clear();
+        charSet.clear()
+    }
+
+    return prio
+}
+
+fun solve2(lines: List<String>): Int {
+    var prio = 0
+    val itemMap = mutableMapOf<Char, Int>()
+    
+    for(elfGroup in lines.chunked(3)) {
+        val first = elfGroup[0]
+        val second = elfGroup[1]
+        val third = elfGroup[2]
+
+        for(c in first) {
+            itemMap.put(c, 0)
+        }
+        
+        for(c in second) {
+            if(itemMap.containsKey(c)) {
+                itemMap[c] = 1
+            }
+        }
+        
+        for(c in third) {
+            if(itemMap.containsKey(c) && itemMap[c] == 1) {
+                prio += toPrio(c)
+                break
+            }
+        }
+
+        itemMap.clear()
     }
     
-    return priorities.sum();
+    return prio
 }
 
 fun main() {
     val lines = readLinesFromFile("input.txt")
-    println("Part 1: ${solve(lines)}");
+    println("Part 1: ${solve(lines)}")
+    println("Part 2: ${solve2(lines)}")
 }
